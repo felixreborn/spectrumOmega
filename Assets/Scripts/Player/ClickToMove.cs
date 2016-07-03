f0cbc333ca7348f646d5d2162401f383579d139a
felixreborn <tombattcock2@hotmail.com>
2016-07-03 21:31:20 +0100

    inital changes

---
+++ Assets/Scripts/Player/ClickToMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+	// Use this for initialization
+	private Vector3 target;
+	private float speed = 5;
+	private bool moveTo = false;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	void Move (){
+		if (Input.GetMouseButtonUp(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()){
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast(ray, out hit, 2000)){
+				target = hit.point;
+				// Debug.Log("Where i want to go");
+				// Debug.Log(hit.point);
+				// Debug.Log("Where i am");
+				// Debug.Log(transform.position);
+				moveTo = true;
+				//navigationAgent.SetDestination(hit.point);
+			}
+		}
+		if (moveTo == true){
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, target, step);
+		}
+		if (moveTo && (transform.position.x < target.x+0.5 && transform.position.x > target.x-0.5) && (transform.position.z < target.z+0.5 && transform.position.z > target.z-0.5)){
+			Debug.Log("I AM HERE");
+			moveTo = false;
+		}
+		
+	}
+}
---
+++ Assets/Scripts/World/BlockAir.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System;       //Add this line
+  
+[Serializable]      //And this line
+public class BlockAir : Block
+{
+    public BlockAir()
+        : base()
+    {
+    }
+    public override MeshData Blockdata
+        (Chunk chunk, int x, int y, int z, MeshData meshData)
+    {
+        return meshData;
+    }
+    public override bool IsSolid(Block.Direction direction)
+    {
+        return false;
+    }
+}
